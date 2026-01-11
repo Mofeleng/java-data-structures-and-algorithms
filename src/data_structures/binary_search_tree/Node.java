a05135e392a3c4a130b2730647f0f8e3f08f33c4
Mofeleng <mofelengmokgope@gmail.com>
2026-01-11 18:44:32 +0200

    Binary search tree

---
+++ src/data_structures/binary_search_tree/Node.java
@@ -0,0 +1,13 @@
+package data_structures.binary_search_tree;
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+
+
+    }
+}
